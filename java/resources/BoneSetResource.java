0a0138d847dd3ff4f8d4b40b6f4fc28b82e33f6a
Rafagan Abreu <rafagansa@outlook.com>
2014-11-02 12:43:07 -0200

    Alterações glassfish

--- java/resources/BoneSetResource.java
+++ java/resources/BoneSetResource.java
@@ -2,13 +2,10 @@ package resources;
 
 import controllers.BoneSetController;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("bonesets")
